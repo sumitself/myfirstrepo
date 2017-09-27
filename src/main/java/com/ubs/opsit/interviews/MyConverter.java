401863a090805d4bf47aa08a55b28641870310cd
sumityonc <hisumithere@gmail.com>
2017-09-27 20:35:03 +0530

    Add own converter
    Add own converter with some basic junit test cases

---
+++ src/main/java/com/ubs/opsit/interviews/MyConverter.java
@@ -0,0 +1,41 @@
+package com.ubs.opsit.interviews;
+
+public class MyConverter implements TimeConverter {
+	String second; 
+	String hour;
+	String minutes;
+	
+	@Override
+	public String convertTime(String aTime) {
+		String[] values = aTime.split(":");
+		
+		int hour = Integer.parseInt(values[0]);
+		int minute = Integer.parseInt(values[1]);
+		int second = Integer.parseInt(values[2]);
+		StringBuilder value = new StringBuilder();
+		value.append(getSecondString(second)).append("\n");
+		value.append(getRowString(hour/5,4,"R")).append("\n");
+		value.append(getRowString(hour%5,4,"R")).append("\n");
+		value.append(getRowString(minute/5,11,"Y").replaceAll("YYY","YYR")).append("\n");
+		value.append(getRowString(minute%5,4,"Y"));
+		return value.toString();
+	}
+	
+	
+	protected String getRowString(int rowValue,int maxValue, String colorCode){
+		StringBuilder topRowHourString = new StringBuilder();
+		for(int i=1; i<=maxValue;i++){
+			if(i <=rowValue){
+				topRowHourString.append(colorCode);
+			}else{
+				topRowHourString.append("O");
+			}
+		}
+		return topRowHourString.toString();
+	}
+	
+	protected String getSecondString(int second){
+		return second%2 ==0?"Y":"O";
+	}
+
+}
---
+++ src/test/java/com/ubs/opsit/interviews/MyConverterTest.java
@@ -0,0 +1,143 @@
+package com.ubs.opsit.interviews;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class MyConverterTest {
+	MyConverter converter = new MyConverter();
+	
+	@Test
+	public void test_getRowString_ForHourRow_withOneLampIluminated(){
+		//Given
+		int maxLampCount = 4;
+		String colorCode = "R";
+		int noOfIluminatedLamp = 1;
+		
+		//When
+		String actualString = converter.getRowString(noOfIluminatedLamp, maxLampCount, colorCode);
+		
+		//then
+		assertThat(actualString).isEqualTo("ROOO");
+		
+	}
+	
+	@Test
+	public void test_getRowString_ForHourRow_withTwoLampIluminated(){
+		//Given
+		int maxLampCount = 4;
+		String colorCode = "R";
+		int noOfIluminatedLamp = 2;
+		
+		//When
+		String actualString = converter.getRowString(noOfIluminatedLamp, maxLampCount, colorCode);
+		
+		//then
+		assertThat(actualString).isEqualTo("RROO");
+		
+	}
+	
+	@Test
+	public void test_getRowString_ForHourRow_withThreeLampIluminated(){
+		//Given
+		int maxLampCount = 4;
+		String colorCode = "R";
+		int noOfIluminatedLamp = 3;
+		
+		//When
+		String actualString = converter.getRowString(noOfIluminatedLamp, maxLampCount, colorCode);
+		
+		//then
+		assertThat(actualString).isEqualTo("RRRO");
+		
+	}
+	
+	@Test
+	public void test_getRowString_ForHourRow_withFourLampIluminated(){
+		//Given
+		int maxLampCount = 4;
+		String colorCode = "R";
+		int noOfIluminatedLamp = 4;
+		
+		//When
+		String actualString = converter.getRowString(noOfIluminatedLamp, maxLampCount, colorCode);
+		
+		//then
+		assertThat(actualString).isEqualTo("RRRR");
+		
+	}
+	
+	@Test
+	public void test_getRowString_ForMinTopRow_withFirstLampIluminated(){
+		//Given
+		int maxLampCount = 11;
+		String colorCode = "Y";
+		int noOfIluminatedLamp = 1;
+		
+		//When
+		String actualString = converter.getRowString(noOfIluminatedLamp, maxLampCount, colorCode);
+		
+		//then
+		assertThat(actualString).isEqualTo("YOOOOOOOOOO");
+		
+	}
+	
+	@Test
+	public void test_getRowString_ForMinTopRow_withFiveLampIluminated(){
+		//Given
+		int maxLampCount = 11;
+		String colorCode = "Y";
+		int noOfIluminatedLamp = 5;
+		
+		//When
+		String actualString = converter.getRowString(noOfIluminatedLamp, maxLampCount, colorCode);
+		
+		//then
+		assertThat(actualString).isEqualTo("YYYYYOOOOOO");
+		
+	}
+	
+	@Test
+	public void test_getRowString_ForMinTopRow_withElevenLampIluminated(){
+		//Given
+		int maxLampCount = 11;
+		String colorCode = "Y";
+		int noOfIluminatedLamp = 11;
+		
+		//When
+		String actualString = converter.getRowString(noOfIluminatedLamp, maxLampCount, colorCode);
+		
+		//then
+		assertThat(actualString).isEqualTo("YYYYYYYYYYY");
+		
+	}
+	
+
+	@Test
+	public void test_getRowString_ForSecond_withOddValue(){
+		//Given
+		int second = 3;
+		
+		//When
+		String actualString = converter.getSecondString(second);
+		
+		//then
+		assertThat(actualString).isEqualTo("O");
+		
+	}
+	
+	@Test
+	public void test_getRowString_ForSecond_withEvenValue(){
+		//Given
+		int second = 28;
+		
+		//When
+		String actualString = converter.getSecondString(second);
+		
+		//then
+		assertThat(actualString).isEqualTo("Y");
+		
+	}
+	
+
+}
